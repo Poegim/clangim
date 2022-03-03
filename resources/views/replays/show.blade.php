f38e124892d3bf0f2fa4a883c9a158240bdef1d9
poegim <jacekwiniarczyk@gmail.com>
2022-03-03 15:09:09 +0100

    replay design fix

--- resources/views/replays/show.blade.php
+++ resources/views/replays/show.blade.php
@@ -11,7 +11,7 @@
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div class="bg-white overflow-hidden shadow-xl rounded-lg mt-12 {{config('settings.color2')}} dark:text-white">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mt-12 {{config('settings.color2')}} dark:text-white">
                 <div
                     class="py-4 px-2 sm:px-12 border-b border-gray-200 bg-gray-200 lg:flex lg:justify-between text-gray-600 leading-7 font-semibold {{config('settings.color1')}} dark:text-gray-300 dark:border-gray-700">
 
