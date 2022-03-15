6daf1c55e6c97b45874ec43339ede5942f3d49a6
poegim <jacekwiniarczyk@gmail.com>
2022-03-15 10:07:28 +0100

    Email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,19 +3,16 @@
 namespace App\Models;
 
 use App\Http\Traits\HasCountry;
-use App\Models\ClanWars\Game;
 use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Jetstream\HasProfilePhoto;
-use App\Models\ClanWars\GameHomePlayer;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
